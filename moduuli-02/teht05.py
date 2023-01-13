0e877a024371d1a2816cced19af5bc7e6ce816ab
Aarni Manninen <aarniman@metropolia.fi>
2023-01-13 15:28:35 +0200

    Moduuli 2 tehtävä 5 korjaus

--- moduuli-02/teht05.py
+++ moduuli-02/teht05.py
@@ -3,11 +3,8 @@ naula = float(input("Anna naulat \n"))
 luoti = float(input("Anna luodit \n"))
 
 pNaula = leiviska * 20 + naula
-print(pNaula)
 pLuoti = pNaula * 32 + luoti
-print(pLuoti)
 paino = pLuoti * 13.3
-print(paino)
 
 kilo = (int(paino / 1000))
 gramma = (float(paino % 1000))
